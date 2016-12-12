1dc528b6eb3d90cdfb19d2cb5c96b63a844339f8
Bamidele <fisiwizy@gmail.com>
2016-12-12 22:55:43 +0100

    First commit; work in progress

---
+++ js/script.js
@@ -0,0 +1,54 @@
+/*function  makeVisible(){
+    var skillSet = document.getElementById("skillset");
+    var goals = document.getElementById("goals");
+    var my_journey = document.getElementById("my_journey");
+
+    if(id == "skillset_button"){
+       //.document.getElementsByClassName("all_about_me").style.visibility = "hidden";
+        skillSet.id = "visible_about_me";
+        goals.id = "invisible_about_me";
+        myJourney.id = "invisible_about_me";
+
+    }else if(id == "goals_button"){
+        skillSet.id = "invisible_about_me";
+        myJourney.id ="invisible_about_me";
+        goals.id = "visible_about_me";
+    }else if( id == "my_journey_button"){
+        myJourney.id = "visible_about_me";
+        skillSet.id = "invisible_about_me";
+        goals.id = "invisible_about_me";
+    }
+}*/
+
+function makeVisible(id){
+
+    var skillset = document.getElementById("skillset");
+    var goals = document.getElementById("goals");
+    var myJourney = document.getElementById("my_journey");
+
+    if(id == "skillset_button"){
+        skillset.style.visibility = "visible";
+        skillset.style.zIndex = "1"
+        goals.style.visibility = "hidden"
+        myJourney.style.visibility = "hidden";
+        goals.style.zIndex = "-1";
+        myJourney.style.zindex = "-1";
+    }else if(id == "goals_button"){
+        skillset.style.visibility = "hidden";
+        goals.style.visibility = "visible";
+        myJourney.style.visibility = "hidden";
+
+        skillset.style.zIndex = "-1";
+        goals.style.zIndex = "1";
+        myJourney.style.zindex = "-1";
+    }else if(id == "my_journey_button"){
+        skillset.style.visibility = "hidden";
+        goals.style.visibility = "hidden";
+        myJourney.style.visibility = "visible";
+        
+        skillset.style.zIndex = "-1";
+        goals.style.zIndex = "-1";
+        myJourney.style.zindex = "1";
+    }
+
+}
